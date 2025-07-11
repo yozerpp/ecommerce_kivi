621a566773fa78ec22674270eda6b9bcc24f3c89
yozerpp <tipili-yusuf-ozer@hotmail.com>
2025-07-12 01:32:45 +0300

    updated the Dao layer to use joins and selects (includes and projections)

--- Ecommerce.Dao/Iface/IRepository.cs
+++ Ecommerce.Dao/Iface/IRepository.cs
@@ -1,20 +1,25 @@
 ﻿using System.Configuration;
+using System.Linq.Expressions;
 
 namespace Ecommerce.Dao.Iface;
 
 public interface IRepository<Entity> where Entity : class, new()
 {
-    public  List<Entity> All();
+    public List<Entity> All(string[][]? includes = null);
+    public List<TP> All<TP>(Expression<Func<Entity, TP>> select,string[][]? includes = null);
 
-    public  List<Entity> Where(Func<Entity, bool> predicate, int offset=0, int limit = 20, params Func<Entity, object>[] orderBy);
+    public List<Entity> Where(Expression<Func<Entity, bool>> predicate,int offset=0, int limit = 20, Expression<Func<Entity, object>>[]? orderBy=null, string[][]? includes=null);
+    public List<TP> Where<TP>(Expression<Func<Entity,TP>> select,Expression<Func<Entity, bool>> predicate,int offset=0, int limit = 20, Expression<Func<Entity, object>>[]? orderBy=null, string[][]? includes=null);
 
-    public  Entity? Find(Func<Entity, bool> predicate);
-    public bool Exists(Func<Entity, bool> predicate);
-    public  Entity Add(Entity entity);
-
+    public Entity? First(Expression<Func<Entity, bool>> predicate,string[][]? includes=null,Expression<Func<Entity, object>>[]? orderBy=null);
+    public TP? First<TP>(Expression<Func<Entity, TP>> select, Expression<Func<Entity, bool>> predicate,string[][]? includes=null,Expression<Func<Entity, object>>[]? orderBy=null);
+    public bool Exists(Expression<Func<Entity, bool>> predicate, string[][]? includes= null);
+    public Entity Add(Entity entity);
     public Entity Update(Entity entity);
-
-    public  Entity Delete(Entity entity);
-    public void Delete(Func<Entity, bool> predicate);
+    public void Update(Action<Entity> updateAction, Expression<Func<Entity, bool>> predicate, string[][]? includes = null);
+    public Entity Delete(Entity entity);
+    public void Delete(Expression<Func<Entity, bool>> predicate,string[][]? includes =null);
     public void Flush();
+    public Entity Detach(Entity entity);
+    public Entity Merge(Entity entity);
 }
