f274210dbc5a07a3523647686376a44d0b3716f4
yozerpp <tipili-yusuf-ozer@hotmail.com>
2025-08-18 11:36:59 +0300

    feat: Add review statistics partial view and model

---
+++ Ecommerce.WebImpl/Pages/Shared/Dashboard/_ReviewStatistics.cshtml.cs
@@ -0,0 +1,11 @@
+﻿using Ecommerce.Entity;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Ecommerce.WebImpl.Pages.Shared.Dashboard;
+
+public class _ReviewStatistics
+{
+    public ICollection<ProductReview> Reviews { get; init; } =[];
+    public User.UserRole ViewerRole { get; init; } = User.UserRole.Customer;
+    
+}
