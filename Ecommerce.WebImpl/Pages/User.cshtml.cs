34044fb667e05f72db41e9fb9df93b9756db43ca
yozerpp <tipili-yusuf-ozer@hotmail.com>
2025-08-18 15:30:45 +0300

    feat: Add User page with email change functionality

---
+++ Ecommerce.WebImpl/Pages/User.cshtml.cs
@@ -0,0 +1,22 @@
+﻿using Ecommerce.Bl.Interface;
+using Ecommerce.Dao.Spi;
+using Ecommerce.WebImpl.Pages.Shared;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Ecommerce.WebImpl.Pages;
+
+[Authorize(Roles = nameof(User))]
+public class User : BaseModel
+{
+    private readonly IJwtManager _jwtManager;
+
+    public User(IJwtManager jwtManager) {
+        _jwtManager = jwtManager;
+    }
+
+    public IActionResult OnPostChangeEmail() {
+        var email = CurrentUser.Email;
+    }
+}
