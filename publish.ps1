cf5a030d8188e8f6a2e35c5c6509eda126186118
yozerpp <tipili-yusuf-ozer@hotmail.com>
2025-09-13 00:18:20 +0300

    obj files

---
+++ publish.ps1
@@ -0,0 +1,4 @@
+﻿dotnet publish Ecommerce.WebImpl --os linux -a x64 -c Release --no-self-contained
+ssh root@185.223.77.167 bash -c "/root/shutdown.sh"
+wsl -e bash "./publish.sh"
+ssh root@185.223.77.167 bash -c "/usr/local/server/start.sh"
---
+++ publish.sh
@@ -0,0 +1,9 @@
+﻿#!/bin/bash
+cd "/mnt/c/Users/tipil/RiderProjects/Ecommerce/Ecommerce.WebImpl/bin/Release/net9.0/linux-x64/publish"
+echo "compressing files"
+tar -cjf dll.tar.bz2 $(ls | egrep -v -e '^wwwroot$')
+tar -cjf wwwroot.tar.bz2 wwwroot/
+echo "decompressing files"
+scp -r *.tar.bz2 root@185.223.77.167:/usr/local/server/
+ssh root@185.223.77.167 tar -xjf /usr/local/server/wwwroot.tar.bz2 -C /usr/local/server/
+ssh root@185.223.77.167 tar -xjf /usr/local/server/dll.tar.bz2 -C /usr/local/server/
