f069eb185e255047fb99e3800867e97cd9d60278
Yusuf Özer <tipili-yusuf-ozer@hotmail.com>
2025-09-13 08:56:58 +0300

    Replace hardcoded server IP with variable

--- publish.ps1
+++ publish.ps1
@@ -1,4 +1,4 @@
 ﻿dotnet publish Ecommerce.WebImpl --os linux -a x64 -c Release --no-self-contained
-ssh root@185.223.77.167 bash -c "/root/shutdown.sh"
+ssh root@$PERSONAL_SERVER bash -c "/root/shutdown.sh"
 wsl -e bash "./publish.sh"
-ssh root@185.223.77.167 bash -c "/usr/local/server/start.sh"
+ssh root@PERSONAL_SERVER bash -c "/usr/local/server/start.sh"
